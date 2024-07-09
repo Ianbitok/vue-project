23cb27385b61e3d408f03aa4f270bc41ebc36a90
ian <ianbitok6465@gmail.com>
2024-07-10 00:12:34 +0300

    vue project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-toolbar color="green">
+    <v-tab to="/">Home</v-tab>
+    <v-tab to="/about">About</v-tab>
+    <v-tab to="/contact">Contact</v-tab>
+    <v-tab to="/login">Login</v-tab>
+    <v-tab to="/signup">Sign Up</v-tab>
+  </v-toolbar>
+        <RouterView/>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+</script>
+
+<template>
+  <v-btn @click="store.increment">increment</v-btn>
+  Current Count: {{ store.count }}
+  <v-btn @click="store.decrement">Decrement</v-btn>
+</template>
+<style scoped></style>
